e46f5936f0ea6473c711e159fed38c74cd70869d
Adria Blancafort <adriablancafort@gmail.com>
2024-09-24 20:31:44 +0200

    detect amazon captcha screen

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-import asyncio
 from curl_cffi.requests import AsyncSession
 from selectolax.parser import HTMLParser
+import asyncio
 
 
-async def request_content(URL: str) -> str | None:
+async def request_content(session: AsyncSession, URL: str) -> str | None:
     """Request the content of a URL and return it as a string."""
 
-    async with AsyncSession() as session:
-        response = await session.get(URL, impersonate="safari_ios")
-        if response.status_code != 200:
-            print(f"Error: {response.status_code}")
-            return None
-        return response.text
+    response = await session.get(URL, impersonate="safari")
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}, URL: {URL}")
+        return None
+    return response.text
 
 
-async def scrape_amazon_product(ASIN: str) -> None:
+async def scrape_amazon_product(session: AsyncSession, ASIN: str) -> None:
     """Scrape the price of an Amazon product given its ASIN."""
-    
+
     URL = f"https://www.amazon.com/dp/{ASIN}"
 
-    html = await request_content(URL)
+    html = await request_content(session, URL)
+
     if html is None:
         return
 
     tree = HTMLParser(html)
 
-    title_element = tree.css_first("span#title")
+    captcha_title = tree.css_first("h4")
+    if captcha_title and "Enter the characters you see below" in captcha_title.text():
+        print(f"Error: CAPTCHA, URL: {URL}")
+        return
+
+    title_element = tree.css_first("h1 span#productTitle")
     price_symbol_element = tree.css_first("span.a-price-symbol")
     price_whole_element = tree.css_first("span.a-price-whole")
     price_fraction_element = tree.css_first("span.a-price-fraction")
@@ -42,11 +47,11 @@ async def scrape_amazon_product(ASIN: str) -> None:
 
 
 async def main():
-    ASINs = ["B09LNW3CY2", "B009KYJAJY", "B0B2D77YB8", "B0D3KPGFHL"]
-    tasks = [scrape_amazon_product(ASIN) for ASIN in ASINs]
-    await asyncio.gather(*tasks)
+    ASINs = ["B0BXPFNNF9", "B0D3KPGFHL", "B0B2D77YB8"]
+    async with AsyncSession() as session:
+        tasks = [scrape_amazon_product(session, ASIN) for ASIN in ASINs]
+        await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
